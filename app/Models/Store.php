d8f7909fe10e87907d1e6cab6be77ea432d30e70
Taiki Kato <tensainanoda@gmail.com>
2024-01-20 11:54:52 +0900

    Store モデルに fillable を追加

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Store extends Model
 {
     use HasFactory;
+
+    protected  $fillable = [
+        'name',
+        'category_id',
+        'discription',
+        'time',
+        'close_time',
+        'price_range',
+        'postal_code',
+        'address',
+        'phone_number',
+        'holiday',
+    ];
 }
