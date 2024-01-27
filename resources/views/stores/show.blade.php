f9a9acce70e6f31d44b6696b7d55a73e497ca516
Taiki Kato <tensainanoda@gmail.com>
2024-01-27 11:36:10 +0900

    お気に入りリクエスト変更

--- resources/views/stores/show.blade.php
+++ resources/views/stores/show.blade.php
@@ -89,18 +89,18 @@
         <button type="submit">レビューを追加</button>
     </form>
 
-    @if(Auth::user()->favorites->contains($store->id))
-    <a href="{{ route('stores.favorite', $store) }}" class="btn nagoyameshi-favorite-button text-favorite w-100">
-        <i class="fa fa-heart"></i>
-        お気に入り解除
-    </a>
-    @else
-    <a href="{{ route('stores.favorite', $store) }}" class="btn nagoyameshi-favorite-button text-favorite w-100">
-        <i class="fa fa-heart"></i>
-        お気に入り
-    </a>
-    @endif
-
+    
+    <form action="{{ route('stores.favorite', $store) }}" method="POST">
+        @csrf
+        <button type="submit" class="btn nagoyameshi-favorite-button text-favorite w-100">
+            <i class="fa fa-heart"></i>
+            @if(Auth::user()->favorites->contains($store->id))
+                お気に入り解除
+            @else
+                お気に入り
+            @endif
+        </button>
+    </form>
 @endauth
 @endsection
 
