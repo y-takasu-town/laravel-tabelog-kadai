3a1d0006183cd11dc344075271dd66c971eb898d
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-17 23:28:34 +0900

    Update mypage.blade.php

--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -37,10 +37,10 @@
             <div class="card-body">
                 <ul>
                     <li>
-                        <a href="{{route('mypage.edit')}}">会員情報の編集</a>
+                        <a href="{{route('subscription')}}">有料会員登録</a>
                     </li>
                     <li>
-                        <a href="{{route('subscription')}}">有料会員登録</a>
+                        <a href="{{route('mypage.edit')}}">会員情報の編集</a>
                     </li>
                     <li>
                         <a href="{{route('mypage.edit_password')}}">パスワード変更</a>
