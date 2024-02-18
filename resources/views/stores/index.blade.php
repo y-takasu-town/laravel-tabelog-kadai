c709c66d92d6d805bd212685929dbd6c19e2ff68
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-18 22:54:25 +0900

    店舗詳細修正、一覧画面修正

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -41,9 +41,30 @@
                                 <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
                             @endif
                         </a><br>
-                            {{ $store->category->name }}<br> 
-                            {{ $store->address }}<br>
-                            {{ $store->discription }}<br>
+                            <div class="row p-2 border-bottom">
+                                <div class="col-2">
+                                    <span class="fw-bold">カテゴリ:</span>
+                                </div>
+                                <div class="col">
+                                    {{$store->category->name}}
+                                </div>
+                            </div><br> 
+                            <div class="row p-2 border-bottom">
+                                <div class="col-2">
+                                    <span class="fw-bold">住所:</span>
+                                </div>
+                                <div class="col">
+                                    <span>{{$store->address}} </span>
+                                </div>
+                            </div><br>
+                            <div class="row p-2 border-bottom">
+                                <div class="col-2">
+                                    <span class="fw-bold">店舗説明:</span>
+                                </div>
+                                <div class="col">
+                                    {{$store->discription}}
+                                </div>
+                            </div><br>
                     </div>
                 @endforeach
             </div>
