09ef6edd72b06a3a1ecde65a702193dcabc13c66
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-20 19:24:51 +0900

    Update favorite.blade.php

--- resources/views/users/favorite.blade.php
+++ resources/views/users/favorite.blade.php
@@ -5,31 +5,32 @@
 @endsection
 
 @section('content')
-    <h1>お気に入り</h1>
+    <div class="justify-content-center">
+        <h1>お気に入り</h1>
 
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+        @if(session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
 
-    @foreach ($favorites as $fav)
-        <a href="{{route('stores.show', $fav->store_id)}}">
-            <h5>{{ $fav->store->name }}</h5>
-                @if ($fav->store->image !== "")
-                    <img src="{{ asset($fav->store->image) }}" class="img-thumbnail">
-                @else
-                    <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
-                @endif
-        </a>
+        @foreach ($favorites as $fav)
+            <a href="{{route('stores.show', $fav->store_id)}}">
+                <h5>{{ $fav->store->name }}</h5>
+                    @if ($fav->store->image !== "")
+                        <img src="{{ asset($fav->store->image) }}" class="img-thumbnail">
+                    @else
+                        <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
+                    @endif
+            </a>
 
-         <form action="{{route('stores.favorite', $fav->store_id)}}" method="POST">
-             @csrf
-             <button class="btn btn-outline-danger">
-             お気に入り解除
-             </button>
-         </form>
-         <hr> 
-    @endforeach
-
+            <form action="{{route('stores.favorite', $fav->store_id)}}" method="POST">
+                @csrf
+                <button class="btn btn-primary my-3">
+                お気に入り解除
+                </button>
+            </form>
+            <hr> 
+        @endforeach
+    </div>
 @endsection
