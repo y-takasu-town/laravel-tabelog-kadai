aa2e0feba966542d5f613da0e8f74229473ea155
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-24 02:45:09 +0900

    Update index.blade.php

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -41,9 +41,9 @@
                                 <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
                             @endif
                         </a><br>
-                            {{ $store->category->name }}<br> 
-                            {{ $store->address }}<br>
-                            {{ $store->discription }}<br>
+                            {{ カテゴリ名:$store->category->name }}<br> 
+                            {{ 住所:$store->address }}<br>
+                            {{ 店舗説明:$store->discription }}<br>
                     </div>
                 @endforeach
             </div>
