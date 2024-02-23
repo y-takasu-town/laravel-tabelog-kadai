5a205098146bc71bc10283c5453f4339b1bbb405
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-24 02:20:31 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,7 @@
     <div class="container-fluid top-container">
         <div class="row justify-content-center align-items-center" style="height: 70vh">
             <div class="col-md-6 text-center mx-auto">
-                <h1>左上のNAGOYAMESHIをタップして、名古屋の味を、探そう。</h1>
+                <h1>ようこそ。左上のNAGOYAMESHIをクリック。</h1>
             </div>
         </div>
     </div>
