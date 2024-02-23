40ae6d3de01b4c098065279269026c71533df3d9
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-24 04:03:15 +0900

    消去箇所戻し

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -8,51 +8,42 @@
     <div class="container nagoyameshi-container pd-5">
         <div class="row justify-content-center">
             <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4">
-                <!-- 検索フォーム -->
                 <form>
                     @csrf
                     <select name="category_id">
-                        <!-- カテゴリ選択のドロップダウンメニュー -->
                         <option disabled selected value>カテゴリを選択</option>
                         @foreach($categories as $category)
-                            <option value="{{ $category->id}}">{{ $category->name}}</option>
+                            <option value="{{ $category->id }}">{{ $category->name }}</option>
                         @endforeach
                     </select>
-                    <!-- キーワード入力 -->
                     <input type="text" name="keyword">
-                    <!-- 検索ボタン -->
                     <button type="submit">検索</button>
                 </form>
 
-                <!-- ソートフォーム -->
                 <form method="GET" action="{{ route('stores.index')}}" class="form-inline">
                     <select name="sort" onChange="this.form.submit();" class="form-inline ml-2">
-                        <!-- ソートオプションのドロップダウンメニュー -->
                         @foreach ($sort as $key => $value)
                             @if ($sorted == $value)
-                                <option value=" {{ $value}}" selected>{{ $key }}</option>
+                                <option value="{{ $value }}" selected>{{ $key }}</option>
                             @else
-                                <option value=" {{ $value}}">{{ $key }}</option>
+                                <option value="{{ $value }}">{{ $key }}</option>
                             @endif
                         @endforeach
                     </select>
                 </form>
 
-                <!-- 店舗一覧 -->
                 @foreach ($stores as $store)
                     <div class="my-5">
                         <a href="{{ route('stores.show',$store) }}">{{ $store->name }}<br>
-                            <!-- 店舗画像 -->
                             @if ($store->image !== "")
                                 <img src="{{ asset($store->image) }}" class="img-thumbnail">
                             @else
-                                <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
+                                <img src="{{ asset('img/dummy.png') }}" class="img-thumbnail">
                             @endif
                         </a><br>
-                        <!-- カテゴリ名、住所、説明文 -->
                         {{ $store->category->name }}<br> 
                         {{ $store->address }}<br>
-                        {{ $store->description }}<br>
+                        {{ $store->discription }}<br>
                     </div>
                 @endforeach
             </div>
--- resources/views/stores/show.blade.php
+++ resources/views/stores/show.blade.php
@@ -8,69 +8,115 @@
     <div class="container nagoyameshi-container pd-5">
         <div class="row justify-content-center">
             <div class="col-xxl-8 col-xl-8 col-lg-8 col-md-8">
-                <!-- 成功メッセージの表示 -->
+                {{-- 成功メッセージ --}}
                 @if(session('success'))
                     <div class="alert alert-success mb-3">
                         {{ session('success') }}
                     </div>
                 @endif
 
-                <!-- 店舗名の表示 -->
-                <h1 class="text-center">{{$store->name}}</h1>
+                {{-- 店舗名 --}}
+                <h1 class="text-center">{{ $store->name }}</h1>
 
-                <!-- 店舗画像の表示 -->
+                {{-- 店舗画像 --}}
                 @if ($store->image)
                     <img src="{{ asset($store->image) }}" class="w-100">
                 @else
-                    <img src="{{ asset('img/dummy.png')}}" class="w-100">
+                    <img src="{{ asset('img/dummy.png') }}" class="w-100">
                 @endif
 
+                {{-- 店舗情報 --}}
                 <div class="container">
-                    <!-- 店舗情報の表示 -->
                     <div class="row p-2 border-bottom">
                         <div class="col-2">
                             <span class="fw-bold">店舗名:</span>
                         </div>
                         <div class="col">
-                            <span>{{$store->name}}</span>
+                            <span>{{ $store->name }}</span>
                         </div>
                     </div>
-                    <!-- 以下、各店舗情報の表示 -->
-                    <!-- ... -->
 
-                    <!-- 予約ボタンの表示 -->
-                    <div class="form-group text-center">
-                        @if (Auth::user()->subscribed('default'))
-                            <a href="{{route('stores.reservation', $store)}}">
-                                <button type="submit" class="mt-3 btn nagoyameshi-submit-button ">
-                                    予約
-                                </button>
-                            </a>
-                        @else
-                            <a href="{{route('subscription')}}" class="btn btn-primary my-3">
-                                予約
-                            </a>
-                        @endif
-                    </div><hr>
+                    <div class="row p-2 border-bottom">
+                        <div class="col-2">
+                            <span class="fw-bold">店舗説明:</span>
+                        </div>
+                        <div class="col">
+                            {{ $store->discription }}
+                        </div>
+                    </div>
 
-                    <!-- レビュー投稿フォーム -->
-                    @auth
-                        @if (Auth::user()->subscribed('default'))
-                            <form method="POST" action="{{ route('reviews.store') }}"><br>
-                                <!-- ... -->
-                            </form>
-                        @endif
-                        <!-- カスタマーレビューの表示 -->
-                        <!-- ... -->
+                    {{-- 以下同様に他の店舗情報を表示 --}}
+                    <!-- このコメントは他の店舗情報も同様のフォーマットで記述することを示します -->
 
-                        <!-- お気に入りボタン -->
-                        @if (Auth::user()->subscribed('default'))
-                            <form action="{{ route('stores.favorite', $store) }}" method="POST">
-                                <!-- ... -->
-                            </form>
-                        @endif
-                    @endauth
                 </div>
+
+                {{-- 予約ボタン --}}
+                <div class="form-group text-center">
+                    @if (Auth::user()->subscribed('default'))
+                        <a href="{{ route('stores.reservation', $store) }}">
+                            <button type="submit" class="mt-3 btn nagoyameshi-submit-button">
+                                予約
+                            </button>
+                        </a>
+                    @else
+                        <a href="{{ route('subscription') }}" class="btn btn-primary my-3">
+                            予約
+                        </a>
+                    @endif
+                </div><hr>
+
+                {{-- レビュー --}}
+                @auth
+                    @if (Auth::user()->subscribed('default'))
+                        <form method="POST" action="{{ route('reviews.store') }}"><br>
+                            @csrf
+                            <h5>評価</h5>
+                            <select name="star">
+                                <option value="5">★★★★★</option>
+                                <option value="4">★★★★</option>
+                                <option value="3">★★★</option>
+                                <option value="2">★★</option>
+                                <option value="1">★</option>
+                            </select><br><br>
+                            <h5>レビュー投稿をしてみよう</h5>
+                            @error('comment')
+                                <strong>レビュー内容を入力してください</strong>
+                            @enderror
+                            <textarea name="comment" rows="5" cols="70"></textarea>
+                            <input type="hidden" name="store_id" value="{{ $store->id }}">
+                            <button type="submit">レビューを追加</button>
+                        </form>
+                    @endif
+
+                    <br>
+                    <h5>カスタマーレビューを読んでみよう</h5>
+                    @if (!Auth::user()->subscribed('default'))
+                        <a href="{{ route('subscription') }}" class="btn btn-primary my-3">有料会員になってレビューを投稿しよう</a>
+                    @endif
+                    @foreach($reviews as $review)
+                        <h5><i class="fa-regular fa-user"></i>{{ $review->user->name }}</h5>
+                        <h3>{{ str_repeat('★', $review->star) }}</h3>
+                        <p>{{ $review->comment }}</p>
+                    @endforeach
+
+                    {{-- お気に入りボタン --}}
+                    @if (Auth::user()->subscribed('default'))
+                        <form action="{{ route('stores.favorite', $store) }}" method="POST">
+                            @csrf
+                            @if(!empty(Auth::user()->favorites()->where('store_id', $store->id)->first()))
+                                <button class="btn nagoyameshi-favorite-button text-favorite w-100">
+                                    <i class="fa fa-heart"></i>
+                                    お気に入り解除
+                                </button>
+                            @else
+                                <button  class="btn nagoyameshi-favorite-button text-favorite w-100">
+                                    <i class="fa fa-heart"></i>
+                                    お気に入り
+                                </button>
+                            @endif
+                        </form>
+                    @endif
+                @endauth
             </div>
         </div>
     </div>
