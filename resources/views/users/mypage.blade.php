4ac695902bfc8d9b43ca4d7d5edf4a049f00cc10
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-24 01:58:00 +0900

    Update mypage.blade.php

--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -18,13 +18,13 @@
             </div>
             <div class="card-body">
                 @if (!$user->subscribed('default'))
-                    <h3 class="text-center"><i class="fa-regular fa-user"></i>無料会員</h3><br>
+                    <h1 class="text-center"><i class="fa-regular fa-user"></i>無料会員</h3><br>
                     <p class="text-center">有料会員になると、予約やレビュー投稿、お気に入り登録ができます。</p>
                 @elseif ($user->subscribed('default') && ($user->subscription('default')->ends_at === null || \Carbon\Carbon::parse($user->subscription('default')->ends_at)->lt(now())))
-                    <h3 class="text-center"><i class="fa-solid fa-crown"></i>有料会員</h3><br>
+                    <h1 class="text-center text-gold"><i class="fa-solid fa-crown"></i>有料会員</h3><br>
                     <p class="text-center">予約やレビュー投稿、お気に入り登録ができます。</p>
                 @else
-                    <h3 class="text-center"><i class="fa-solid fa-crown"></i>有料会員</h3><br>
+                    <h1 class="text-center text-gold"><i class="fa-solid fa-crown"></i>有料会員</h3><br>
                     <p class="text-center">{{ \Carbon\Carbon::parse($user->subscription('default')->ends_at)->format('Y年m月d日') }}まで有効です。</p>
                 @endif
             </div>
