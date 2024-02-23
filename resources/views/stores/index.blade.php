0126f38e083c2c704176e15e44e0cfa4ff0c7dd0
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-24 01:44:42 +0900

    Update index.blade.php

--- resources/views/stores/index.blade.php
+++ resources/views/stores/index.blade.php
@@ -2,22 +2,31 @@
 
 @section('css')
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
+    <style>
+        /* 追加したスタイル */
+        .nagoyameshi-container {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh; /* 画面の高さいっぱいに表示するため */
+        }
+    </style>
 @endsection
 
 @section('content')
-    <div class="container nagoyameshi-container pd-5">
+    <div class="container nagoyameshi-container">
         <div class="row justify-content-center">
             <div class="col-xxl-8 col-xl-8 col-lg-8 col-md-8">
                 <form>
                     @csrf
-                        <select name="category_id">
-                            <option disabled selected value>カテゴリを選択</option>
-                                @foreach($categories as $category)
-                                    <option value="{{ $category->id}}">{{ $category->name}}</option>
-                                @endforeach
-                        </select>
-                        <input type="text" name="keyword">
-                        <button type="submit">検索</button>
+                    <select name="category_id">
+                        <option disabled selected value>カテゴリを選択</option>
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id}}">{{ $category->name}}</option>
+                        @endforeach
+                    </select>
+                    <input type="text" name="keyword">
+                    <button type="submit">検索</button>
                 </form>
 
                 <form method="GET" action="{{ route('stores.index')}}" class="form-inline">
@@ -41,9 +50,9 @@
                                 <img src="{{ asset('img/dummy.png')}}" class="img-thumbnail">
                             @endif
                         </a><br>
-                            {{ $store->category->name }}<br> 
-                            {{ $store->address }}<br>
-                            {{ $store->discription }}<br>
+                        {{ $store->category->name }}<br> 
+                        {{ $store->address }}<br>
+                        {{ $store->discription }}<br>
                     </div>
                 @endforeach
             </div>
