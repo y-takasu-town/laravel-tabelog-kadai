f6f0cfb3ddea4e278e86f00c6f1211bba085cd89
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-23 22:22:52 +0900

    マイページ修正

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -89,17 +89,4 @@ class SubscriptionController extends Controller
         return redirect()->route('mypage')->with('message', '支払い方法が更新されました。');
     }
 
-    public function mypage()
-    {
-        $user = Auth::user();
-        $subscriptionEndsAt = null;
-    
-        // 有料会員であれば、有料会員の期限を取得する
-        if ($user->subscribed('default')) {
-            $subscriptionEndsAt = $user->subscription('default')->ends_at;
-        }
-    
-        return view('users.mypage', compact('user', 'subscriptionEndsAt'));
-    }
-
 }
