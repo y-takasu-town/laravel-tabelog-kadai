0f87e42942c38b838b2b39293902ee461711b238
y-takasu-town <144317982+y-takasu-town@users.noreply.github.com>
2024-02-24 19:52:24 +0900

    Update email.blade.php

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,59 +1,57 @@
 <x-mail::message>
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# @lang('Whoops!')
+@else
+# @lang('こんにちは!')
+@endif
+@endif
 
-    {{-- 挨拶 --}}
-    @if (!empty($greeting))
-        # {{ $greeting }}
-    @else
-        @if ($level === 'error')
-            # @lang('おっと！')
-        @else
-            # @lang('こんにちは！')
-        @endif
-    @endif
+{{-- Intro Lines --}}
+@foreach ($introLines as $line)
+{{ $line }}
 
-    {{-- 紹介行 --}}
-    @foreach ($introLines as $line)
-        {{ $line }}
-        
-    @endforeach
+@endforeach
 
-    {{-- アクションボタン --}}
-    @isset($actionText)
-        <?php
-            $color = match ($level) {
-                'success', 'error' => $level,
-                default => 'primary',
-            };
-        ?>
-        <x-mail::button :url="$actionUrl" :color="$color">
-            {{ $actionText }}
-        </x-mail::button>
-    @endisset
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+    $color = match ($level) {
+        'success', 'error' => $level,
+        default => 'primary',
+    };
+?>
+<x-mail::button :url="$actionUrl" :color="$color">
+{{ $actionText }}
+</x-mail::button>
+@endisset
 
-    {{-- 結びの言葉 --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
-        
-    @endforeach
+{{-- Outro Lines --}}
+@foreach ($outroLines as $line)
+{{ $line }}
 
-    {{-- 敬具 --}}
-    @if (!empty($salutation))
-        {{ $salutation }}
-    @else
-        @lang('よろしくお願いします。')<br>
-        {{ config('app.name') }}
-    @endif
+@endforeach
 
-    {{-- サブコピー --}}
-    @isset($actionText)
-        <x-slot:subcopy>
-            @lang(
-                ":actionText ボタンが利用できない場合は、以下のURLをコピー＆ペーストしてブラウザから直接アクセスしてください。\n",
-                [
-                    'actionText' => $actionText,
-                ]
-            ) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-        </x-slot:subcopy>
-    @endisset
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+@lang('よろしくお願いします。')<br>
+{{ config('app.name') }}
+@endif
 
+{{-- Subcopy --}}
+@isset($actionText)
+<x-slot:subcopy>
+@lang(
+      ":actionText ボタンが利用できない場合は、以下のURLをコピー＆ペーストしてブラウザから直接アクセスしてください。\n",
+    [
+        'actionText' => $actionText,
+    ]
+) <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+</x-slot:subcopy>
+@endisset
 </x-mail::message>
